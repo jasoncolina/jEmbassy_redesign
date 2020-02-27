0897f0e7c4869296e29f0727bc0da89fc7fec4d2
jasoncolina <jasoncolina@users.noreply.github.com>
2020-02-27 17:57:55 -0500

    Updated script.js
    Added click listeners for the toggle function. It works! onResize functions coming soon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,25 @@
 var elem = document.querySelector('.carousel');
+var navLinks = document.querySelector('.nav-collapse');
+var toggle = document.querySelector('.toggle');
 
 var flkty = new Flickity( elem, {
-  pageDots: false,
-  prevNextButtons: false,
+  pageDots: true,
+  prevNextButtons: true,
   wrapAround: true,
   autoPlay:5000
+});
+
+toggle.addEventListener('click', function(){
+  if(navLinks.classList.contains('nav-collapse-active')){
+    navLinks.classList.toggle('nav-collapse-show');
+    setTimeout(function(){
+      navLinks.classList.toggle('nav-collapse-active');
+    },300);
+  }
+  else{
+    navLinks.classList.toggle('nav-collapse-active');
+    setTimeout(function(){
+      navLinks.classList.toggle('nav-collapse-show');
+    },10);
+  }
 });
